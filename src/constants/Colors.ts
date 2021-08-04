5159f97b4c632543b46073042b5bf194a68e2789
xxsiders <loayalaa25@gmail.com>
2021-08-04 23:37:35 +0200

    style: :lipstick: add theme constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,13 @@
+
+
+const Colors = {
+  primary: '#4f62b4',
+  secondry: '#edeef8',
+  third: '#e9e8ec',
+  yellow: '#ffe6a8',
+  pink: '#ffe1ea',
+  darkPink: '#bec1e9',
+  lightBlue: '#92e9f1'
+}
+
+export default Colors
