f901121d36c252118b3733090831502f09ff03b3
xxsiders <loayalaa25@gmail.com>
2021-08-04 23:48:18 +0200

    feat: :sparkles: add home page src files

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
+import Home from '../screens/Home';
 
 export default function Navigation() {
   return (
@@ -15,7 +16,7 @@ const Stack = createStackNavigator();
 function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-
+      <Stack.Screen name='Home' component={Home} />
     </Stack.Navigator>
   );
 }
