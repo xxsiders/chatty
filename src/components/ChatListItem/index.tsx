a749b16ff294b32a12fed3dc7d04944c5f70404d
xxsiders <loayalaa25@gmail.com>
2021-08-05 21:16:43 +0200

    feat: :lipstick: create chat list item component

---
+++ src/components/ChatListItem/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import Avatar from '../Avatar'
+import styles from './styles'
+
+export default function index({ color }: { color: string }) {
+
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Avatar size={60} style={{ backgroundColor: color }} />
+            <View style={styles.dataContainer}>
+                <View style={styles.block}>
+                    <Text style={styles.username}>Username</Text>
+                    <Text style={styles.date}>date</Text>
+                </View>
+                <Text numberOfLines={1} style={styles.message}>Last message is send tyo you before i will</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
---
+++ src/components/ChatListItem/styles.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+import Layout from "../../constants/Layout";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: Layout.styles.flexDirection,
+        paddingHorizontal: '5%',
+        marginBottom: 24,
+        justifyContent: "space-between",
+        alignItems: 'center'
+    },
+    dataContainer: {
+        flex: 1,
+        marginHorizontal: 12,
+        overflow: 'hidden'
+    },
+    block: {
+        flexDirection: Layout.styles.flexDirection,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 4
+    },
+    date: {
+        fontSize: Layout.fonts.h5,
+        color: Colors.gray
+    },
+    username: {
+        fontSize: Layout.fonts.h3,
+        fontWeight: 'bold',
+        color: Colors.foruth
+    },
+    message: {
+        fontSize: Layout.fonts.h5,
+        color: Colors.foruth,
+    }
+})
+
+export default styles
