afb5242e0a418e566c8bc3b0025315ab0873b373
xxsiders <loayalaa25@gmail.com>
2021-08-05 21:19:00 +0200

    feat: :lipstick: update home ui with components

--- src/screens/Home/Home/index.tsx
+++ src/screens/Home/Home/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import ChatList from '../../../components/ChatList'
+import UserList from '../../../components/UserList'
+import styles from './styles'
 
+const dumData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 export default function index() {
+
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <View style={[styles.headerContainer, { paddingTop: useSafeAreaInsets().top + 6 }]}>
+                <Text style={styles.title}>Chat with your friends</Text>
+                <UserList data={dumData} />
+            </View>
+            <View style={styles.contentContainer}>
+                <ChatList data={dumData} />
+            </View>
         </View>
+
     )
 }
--- src/screens/Home/Home/styles.tsx
+++ src/screens/Home/Home/styles.tsx
@@ -1,7 +1,30 @@
 import { StyleSheet } from "react-native";
+import Colors from "../../../constants/Colors";
+import Layout from "../../../constants/Layout";
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: Colors.primary,
+    },
+    contentContainer: {
+        backgroundColor: Colors.white,
+        flex: 1,
+        borderTopEndRadius: 50,
+        borderTopStartRadius: 50
+    },
+    headerContainer: {
+        paddingVertical: 20,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: Layout.fonts.h1,
+        color: Colors.white,
+        fontWeight: 'bold',
+        width: 180,
+        marginHorizontal: '10%',
+        marginBottom: 16,
+    },
 })
 
 export default styles
