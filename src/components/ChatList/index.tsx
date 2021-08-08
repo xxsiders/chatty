9cdced301bfa1a33e93616938d8b065d24adb657
xxsiders <loayalaa25@gmail.com>
2021-08-08 20:30:09 +0200

    feat: :sparkles: update chatlist to accpet data

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FlatList } from 'react-native'
+import { ChatRoom } from '../../../Types'
 import GenerateColors from '../../utils/GenerateColors'
 import ChatListItem from '../ChatListItem'
 import styles from './styles'
-export default function index({ data }: { data: Array<any> }) {
+export default function index({ data }: { data: Array<ChatRoom> }) {
     const avatarColors = GenerateColors(data.length)
 
-    const renderItem = ({ item, index }: any) => {
-        return <ChatListItem color={avatarColors[index]} />
+    const renderItem = ({ item, index }: { item: ChatRoom, index: number }) => {
+        return <ChatListItem data={item} color={avatarColors[index]} />
     }
     return (
         <FlatList data={data}
