da9bac445d31bd19e5b560d44fa56c7df51768a0
xxsiders <loayalaa25@gmail.com>
2021-08-08 20:31:09 +0200

    feat: :sparkles: update userList to accept data

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -5,17 +5,18 @@ import Colors from '../../constants/Colors'
 import GenerateColors from '../../utils/GenerateColors'
 import styles from './styles'
 import Avatar from '../Avatar'
+import { User } from '../../../Types'
 
 
-export default function index({ data }: { data: Array<any> }) {
+export default function index({ data }: { data: Array<User> }) {
     const avatarColors = GenerateColors(data.length)
-    const renderUser = ({ item, index }: any) => {
+    const renderUser = ({ item, index }: { item: any, index: number }) => {
         if (item.searchIcon) {
             return <TouchableOpacity style={styles.searchIcon}>
                 <Ionicons name='search' size={24} color={Colors.white} />
             </TouchableOpacity>
         }
-        return <Avatar style={{ marginHorizontal: 4, backgroundColor: avatarColors[index] }} uri={item} size={45} />
+        return <Avatar style={{ marginHorizontal: 4, backgroundColor: avatarColors[index] }} uri={item.avatar} size={45} />
     }
 
     return (
